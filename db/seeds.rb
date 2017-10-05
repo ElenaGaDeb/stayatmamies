0d5f449a15ae50fd793b842e53e6cbea2a314106
elena garre debies <elena.garredebies@edhec.com>
2017-10-05 10:34:31 +0200

    creation of main seeds for test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u1 = User.new(
+  email: "mamie@edhec.edu",
+  password: "mamieedhec"
+)
+profile1 = Profile.new(user: u1,
+  type: "student",
+  username: "totolebon",
+  first_name: "toto",
+  last_name: "lebon",
+  age: 23,
+  city_from: "Melbourne",
+  gender: "male",
+  detailed_description: "je m'appelle toto, j'habite à Melbourne et j'aime voyager"
+  short_description: "Toto cherche appartement"
+)
+appart1 = Apartment.new(name: 'sunny beedroom in Paris',
+  description: 'appartement dans paris cherche jeune fille calme',
+  deal: '3 soirs de libres par semaine + marché dimanche',
+  street: 'rue Freycinet',
+  city: 'Paris',
+  zip_code: 75116,
+  country: 'France',
+  bedrooms: 1,
+  bathrooms: 1,
+  street2: 'rdc'
+)
+
+

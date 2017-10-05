15bb41b4f3c2b577fa95868715e031da1ba63c48
Elies Gaillard <elies.gaillard@gmail.com>
2017-10-05 19:11:19 +0200

    review controller: added before_action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,10 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
   def index
+    @reviews = Review.all
   end
 
   def new
+    @review = Review.new
   end
 end

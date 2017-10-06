b79e7f3e6dccd40df5d6c6be6c0e357c4122fb21
Auriane de Premilhat <adeprem@vt.edu>
2017-10-06 15:29:56 +0200

    changed the root page to apartments#index, added a become a host link in the navbar, created a banner for the home page- waiting on all the models/ controllers to be functionnal before I do the cards for the apartments

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :apartments, only: [:show, :index, :new, :create, :edit, :update]
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root to: 'pages#home'
+  root to: 'apartments#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

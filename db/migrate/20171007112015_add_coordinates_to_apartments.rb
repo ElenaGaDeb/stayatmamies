4e6d0847cbb342e5923444d697e2ce6d1f43f07f
Auriane de Premilhat <adeprem@vt.edu>
2017-10-07 13:58:16 +0200

    trying to fix heroku

--- db/migrate/20171007112015_add_coordinates_to_apartments.rb
+++
@@ -1,6 +0,0 @@
-class AddCoordinatesToApartments < ActiveRecord::Migration[5.1]
-  def change
-    add_column :apartments, :latitude, :float
-    add_column :apartments, :longitude, :float
-  end
-end

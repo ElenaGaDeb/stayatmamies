21278076c4966f7b0bd76a2a762ddf0624ae7e95
Auriane de Premilhat <31307153+adeprem@users.noreply.github.com>
2017-10-07 13:53:04 +0200

    Delete 20171007102427_add_infos_to_profiles.rb

--- db/migrate/20171007102427_add_infos_to_profiles.rb
+++
@@ -1,6 +0,0 @@
-class AddInfosToProfiles < ActiveRecord::Migration[5.1]
-  def change
-    add_column :profiles, :phone_number, :integer
-    add_column :profiles, :country_from, :string
-  end
-end

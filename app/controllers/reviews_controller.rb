fa18bba72e89ef1013434491cf59b57d6946e291
Elies Gaillard <elies.gaillard@gmail.com>
2017-10-07 18:30:19 +0200

    reviews - les errors messages ne sont plus cassés - AJAX non commencé

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,12 +7,13 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @user = User.find(params[:user_id])
+    @profile = @user.profile
     @review.by_user_id = current_user.id
     @review.for_user_id = @user.id
     if @review.save
       redirect_to profile_path(@user.profile)
     else
-      redirect_to profile_path(@user.profile)
+      render 'profiles/show'
     end
   end
 

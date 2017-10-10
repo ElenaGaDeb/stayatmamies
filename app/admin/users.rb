fd602ae915806b03ea1da8ca528cf2cc2ae818da
Elies Gaillard <elies.gaillard@gmail.com>
2017-10-10 11:39:21 +0200

    added a form for USER new - fix needed - the submit button doesn't create a new user

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,6 +1,4 @@
 ActiveAdmin.register User do
-  permit_params :email, :first_name, :last_name
-
   #remove useless columns in admin interface
   index do
     selectable_column
@@ -12,4 +10,18 @@ ActiveAdmin.register User do
     column :admin
     actions
   end
+
+  form do |f|
+    f.inputs "Identity" do
+      f.input :first_name
+      f.input :last_name
+      f.input :email
+    end
+    f.inputs "Admin" do
+      f.input :admin
+    end
+    f.actions
+  end
+
+  permit_params :first_name, :last_name, :email
 end

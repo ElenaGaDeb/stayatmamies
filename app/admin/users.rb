9f59f68df776735b375754592a51a242578799c2
Elies Gaillard <elies.gaillard@gmail.com>
2017-10-10 11:27:25 +0200

    removed useless columns in USER admin interface

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register User do
   permit_params :email, :first_name, :last_name
+
+  #remove useless columns in admin interface
+  index do
+    selectable_column
+    column :id
+    column :email
+    column :first_name
+    column :last_name
+    column :created_at
+    column :admin
+    actions
+  end
 end

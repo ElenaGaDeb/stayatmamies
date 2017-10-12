39b1853827e8c054578b00c245b9031511523c78
Elies Gaillard <elies.gaillard@gmail.com>
2017-10-12 14:34:02 +0200

    added secrets.yml to gitignore, removed extra form in reviews, changed pundit so you can only review someone who shares a booking with you

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,7 +5,7 @@ class ReviewsController < ApplicationController
 
 
    # Uncomment when you *really understand* Pundit!
-   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
    def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(profile_path(@profile))

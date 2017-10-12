6e813c36a23fefc2f3045b56c2dc9b04e7dc1c48
wenzhu chen <wenzhu.chen1@gmail.com>
2017-10-12 19:42:13 +0200

    added view for conversations & added conversation reference to bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :apartment
   belongs_to :user
   has_many :messages
+  belongs_to :conversation
 end
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -3,6 +3,7 @@ class Conversation < ApplicationRecord
   belongs_to :apartment
   belongs_to :sender, foreign_key: :sender_id, class_name: "User"
   belongs_to :recipient, foreign_key: :recipient_id, class_name: "User"
+  has_many :bookings
 
 
   validates :sender_id, uniqueness: { scope: [:recipient_id, :apartment_id] }

8f8ffc24405ee3702fc8f6eadbf2f74f95c508ad
Elies Gaillard <elies.gaillard@gmail.com>
2017-10-12 19:17:03 +0200

    review form is hidden if review already posted, in apartment#show + profile#show

--- app/policies/review_policy.rb
+++ app/policies/review_policy.rb
@@ -7,10 +7,10 @@ class ReviewPolicy < ApplicationPolicy
 
   def create?
     #    (user.profile.profile_type == 'student' && User.find(record.for_user_id).profile.profile_type == 'host') || (user.profile.profile_type == 'host' && User.find(record.for_user_id).profile.profile_type == 'student')
-    if user.profile.profile_type == 'student'
-      return user.bookings.map { |b| Apartment.find(b.apartment_id).user_id }.include?(record.for_user_id)
-    else
-      return user.apartment.bookings.map { |b| b.user_id }.include?(record.for_user_id)
-    end
+      if user.profile.profile_type == 'student'
+        return user.bookings.map { |b| Apartment.find(b.apartment_id).user_id }.include?(record.for_user_id)
+      else
+        return user.apartment.bookings.map { |b| b.user_id }.include?(record.for_user_id)
+      end
   end
 end
